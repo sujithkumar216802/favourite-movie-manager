513dd1add8b3f2e7c9eaffc71b2f1d11592d1aad
sujithkumar216802 <sujithkumar216802@gmail.com>
2022-11-27 00:37:42 +0530

    refactor: using parameters in db query

--- db/User.js
+++ db/User.js
@@ -1,11 +1,11 @@
 const db = require('./db');
 
 async function queryUserByEmail(email) {
-    return await db.query(`SELECT * FROM users WHERE email=\'${email}\';`);
+    return await db.query(`SELECT * FROM users WHERE email=$1;`, [email]);
 }
 
 async function createUser(email, password) {
-    return await db.query(`INSERT INTO users (email, password) VALUES (\'${email}\', \'${password}\');`);
+    return await db.query(`INSERT INTO users (email, password) VALUES ($1, $2);`, [email, password]);
 }
 
 module.exports = {
--- routes/Login.js
+++ routes/Login.js
@@ -2,6 +2,6 @@ const express = require('express');
 const User = require('../controllers/User');
 const router = express.Router();
 
-router.get('/login', User.loginUser);
+router.post('/login', User.loginUser);
 
 module.exports = router;
